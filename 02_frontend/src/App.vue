924ce8a75db25851719c4e95f3b2ace91df4e9a4
PSE22 <114322247+PSE22@users.noreply.github.com>
2024-04-11 10:56:57 +0900

    first commit
    first commit

---
+++ 02_frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 헤더 -->
+  <HeaderCom />
+  
+  <!-- 본문 -->
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HeaderCom from "./components/common/HeaderCom.vue";
+
+export default {
+  components: {
+    HeaderCom
+  }
+}
+</script>
+
+<style>
+@import "@/assets/css/app.css";
+</style>
