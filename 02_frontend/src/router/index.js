662c3d06be5f9142f2e041252843bdc43717d3e7
PSE22 <114322247+PSE22@users.noreply.github.com>
2024-04-16 20:40:15 +0900

    Update LoginView (#28)
    * branch test
    
    * Create LoginView

--- 02_frontend/src/router/index.js
+++ 02_frontend/src/router/index.js
@@ -1,19 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
+  // Home
   {
-    path: '/',
-    component: () => import('../views/HomeView.vue')
+    path: "/",
+    component: () => import("../views/HomeView.vue"),
   },
+  // 로그인
+  {
+    path: "/login",
+    component: function () {
+      return import("../views/LoginView.vue");
+    },
+  },
+  // 아이디 찾기
+  {
+    path: "/find-id",
+    component: function () {
+      return import("../views/IdView.vue");
+    },
+  },
+  // 비밀번호 찾기
+  {
+    path: "/find-password",
+    component: function () {
+      return import("../views/PasswordView.vue");
+    },
+  },
+  // 회원가입
+  {
+    path: "/register",
+    component: function () {
+      return import("../views/RegisterView.vue");
+    },
+  },
+  // 마이페이지
   {
     path: "/mypage",
     component: () => import("../views/MyPageMain.vue"),
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
