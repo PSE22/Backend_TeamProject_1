53e667f41082a15ccc21c21ef2432c277e88b3fa
toveny1 <161279906+toveny1@users.noreply.github.com>
2024-04-16 20:13:09 +0900

    Dev hj (#27)
    * Create Test2.txt
    
    * HJ
    
    * Update Test2.txt
    
    * Delete Test2.txt
    
    * 1
    
    * Update README.md
    
    * mypage_update_1

--- 02_frontend/src/views/MyPageMain.vue
+++ 02_frontend/src/views/MyPageMain.vue
@@ -1,16 +1,23 @@
 <template>
   <MyPageMain />
-  <div class="container">
-    <div class="my-page-title-box">
-      <div>
-        <ul>
-          <li>주문내역</li>
-          <li>|</li>
-          <li>쿠폰</li>
-          <li>|</li>
-          <li>적립금</li>
-        </ul>
-      </div>
+  <div class="myInfoWrap">
+    <div class="mypageTitle">
+      <h4 class="title">My 주문정보</h4>
+      <p>최근 15일기준</p>
+    </div>
+    <div class="orderInfoBox">
+        <li class="type1">
+          <strong>0</strong>
+          <span>결제완료</span></li>
+        <li class="type2">
+          <strong>0</strong>
+          <span>상품준비중</span></li>
+        <li class="type3">
+          <strong>0</strong>
+          <span>배송중</span></li>
+        <li class="type4">
+          <strong>0</strong>
+          <span>배송완료</span></li>
     </div>
   </div>
 </template>
