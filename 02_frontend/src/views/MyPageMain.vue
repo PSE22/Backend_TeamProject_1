38e3a1b8cbb362eb9d561bd64334fcc5b14daf1d
toveny1 <toveny1@naver.com>
2024-04-16 18:07:23 +0900

    Update myPage

--- 02_frontend/src/views/MyPageMain.vue
+++ 02_frontend/src/views/MyPageMain.vue
@@ -1,18 +1,31 @@
 <template>
   <MyPageMain />
-  <div class="container">
-    <div class="my-page-title-box">
-      <div>
-        <ul>
-          <li>주문내역</li>
-          <li>|</li>
-          <li>쿠폰</li>
-          <li>|</li>
-          <li>적립금</li>
-        </ul>
+  <div class="maininfowrap">
+    <div class="mypageTitle">
+        <h2 class="title">My 주문정보</h2>
+        <p>최근 15일기준</p>
+      </div>
+      <div class="orderInfoBox">
+        <!-- <ul> -->
+          <li class="type1">
+            <strong>0</strong>
+            <span>결제완료</span>
+          </li>
+          <li class="type2">
+            <strong>0</strong>
+            <span>상품준비중</span>
+          </li>
+          <li class="type3">
+            <strong>0</strong>
+            <span>배송중</span>
+          </li>
+          <li class="type4">
+            <strong>0</strong>
+            <span>배송완료</span>
+          </li>
+        <!-- </ul> -->
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -24,4 +37,5 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import "@/assets/css/mypage.css";</style>
