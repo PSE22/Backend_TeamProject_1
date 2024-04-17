773156a1dcd0a07422a4808a8f7386928505cf99
PSE22 <shieun45@gmail.com>
2024-04-17 20:16:54 +0900

    Update 카테고리 탭

--- 02_frontend/src/components/common/HeaderCom.vue
+++ 02_frontend/src/components/common/HeaderCom.vue
@@ -29,23 +29,41 @@
   <div class="container text-center main-nav2">
     <div class="row">
       <div class="col p-0">
-        <router-link to="/category" class="router-link category-menu">카테고리</router-link>
+        <div class="category-menu" id="category-tab" @click="toggleCategory">카테고리
+          <ul v-if="categoryTab">
+            <router-link to="/category/1" class="router-link"><li>카테고리1</li></router-link>
+            <router-link to="/category/2" class="router-link"><li>카테고리2</li></router-link>
+            <router-link to="/category/3" class="router-link"><li>카테고리3</li></router-link>
+            <router-link to="/category/4" class="router-link"><li>카테고리4</li></router-link>
+          </ul>
+        </div>
       </div>
       <div class="col p-0">
         <router-link to="/best-item" class="router-link category-menu">베스트</router-link>
       </div>
       <div class="col p-0">
-        <router-link to="/event" class="router-link category-menu">이벤트</router-link>
+        <router-link to="/new-item" class="router-link category-menu">신상품</router-link>
       </div>
       <div class="col p-0">
-        <router-link to="/customer" class="router-link category-menu">고객지원</router-link>
+        <router-link to="/event" class="router-link category-menu">이벤트</router-link>
       </div>
     </div>
   </div>
 
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      categoryTab: false,
+    }
+  },
+  methods: {
+    toggleCategory() {
+      this.categoryTab = !this.categoryTab;
+    }
+  },
+};
 </script>
 
 <style>
