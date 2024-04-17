5f0226cc74c1b0898598267813effcde2ab00f61
toveny1 <161279906+toveny1@users.noreply.github.com>
2024-04-17 18:38:38 +0900

    updateMyPage (#43)

--- 02_frontend/src/components/mypage/MyPageMainMenu.vue
+++ 02_frontend/src/components/mypage/MyPageMainMenu.vue
@@ -1,19 +1,15 @@
 // 마이페이지 메인
 <template>
-  <div>
-    <div class="my-page container">
+    <div class="my-page">
       <div class="my-page-title">마이페이지</div>
-      <div>
-        <li class="my-page-menu"><a href="#">주문/배송 조회</a></li>
-        <li class="my-page-menu2"><a href="#">쿠폰함</a></li>
-        <li class="my-page-menu2"><a href="#">마일리지</a></li>
-        <li class="my-page-menu2"><a href="#">위시리스트</a></li>
-        <li class="my-page-menu2"><a href="#">1:1 문의내역</a></li>
-        <li class="my-page-menu2"><a href="#">리뷰 관리</a></li>
-        <li class="my-page-menu2"><a href="#">회원정보 수정</a></li>
-      </div>
+        <li class="my-page-menu"><router-link to="/order" class="router-link">주문/배송조회</router-link></li>
+        <li class="my-page-menu2"><router-link to="/" class="router-link">쿠폰함</router-link></li>
+        <li class="my-page-menu2"><router-link to="/" class="router-link">마일리지</router-link></li>
+        <li class="my-page-menu2"><router-link to="/" class="router-link">위시리스트</router-link></li>
+        <li class="my-page-menu2"><router-link to="/" class="router-link">1:1 문의내역</router-link></li>
+        <li class="my-page-menu2"><router-link to="/" class="router-link">리뷰 관리</router-link></li>
+        <li class="my-page-menu2"><router-link to="/" class="router-link">회원정보 수정</router-link></li>
     </div>
-  </div>
 </template>
 
 <script>
