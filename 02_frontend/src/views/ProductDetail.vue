a4e88e9fe65613f2cf78b2c9c23f4bca5e3e5c7c
sjuk265 <161279921+sjuk265@users.noreply.github.com>
2024-04-18 10:07:07 +0900

    Update 상품 상세 (#47)
    Co-authored-by: PSE22 <114322247+PSE22@users.noreply.github.com>

---
+++ 02_frontend/src/views/ProductDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+  <div id="product-image">
+    <img src="https://via.placeholder.com/500x500?text=Image" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">상품 설명</p>
+    </div>
+  </div>
+  <div id="product-text">
+    <div>상품명</div>
+  </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
