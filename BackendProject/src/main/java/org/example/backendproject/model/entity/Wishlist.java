11d77821c3743c55f41d367fec2b3f86369ea79f
toveny1 <161279906+toveny1@users.noreply.github.com>
2024-04-22 18:05:37 +0900

    mypageupdate (#79)

--- BackendProject/src/main/java/org/example/backendproject/model/entity/Wishlist.java
+++ BackendProject/src/main/java/org/example/backendproject/model/entity/Wishlist.java
@@ -1,5 +1,13 @@
 package org.example.backendproject.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.backendproject.model.common.BaseTimeEntity;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Date;
+
 /**
  * packageName : org.example.backendproject.model.entity
  * fileName : Wishlist
@@ -13,9 +21,30 @@ package org.example.backendproject.model.entity;
  * -----------------------------------------------------------
  * 2024-04-19         SAMSUNG          최초 생성
  */
-public class Wishlist {
+@Entity
+@Table(name = "TB_WISHLIST")
+@SequenceGenerator(
+        name = "SQ_DEPT_GENERATOR"
+        , sequenceName = "SQ_WISHLIST"
+        , initialValue = 1
+        , allocationSize = 1
+)
+@DynamicInsert
+@DynamicUpdate
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Wishlist extends BaseTimeEntity {
 //    pd_id	number
 //    user_id	varchar2(100 byte)
 //    add_date	date
 //    status	char(1 byte)
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE
+        , generator = "SQ_WISHLIST_GENERATOR"
+)
+    private Integer pd_id;
+    private String user_id;
 }
