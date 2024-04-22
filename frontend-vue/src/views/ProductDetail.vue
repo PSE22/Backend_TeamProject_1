c04b9fd19708be3d173d9ad57cf1d45fc4925000
sjuk265 <161279921+sjuk265@users.noreply.github.com>
2024-04-22 18:09:01 +0900

    product datail update (#81)

--- frontend-vue/src/views/ProductDetail.vue
+++ frontend-vue/src/views/ProductDetail.vue
@@ -29,7 +29,7 @@
         </li>
         <li><div id="option-text">옵션 선택</div></li>
         <select name="option" class="select">
-          <option value="1">상품 상세 옵션 선택 1</option>
+          <option value="1" selected>상품 상세 옵션 선택 1</option>
           <option value="2">상품 상세 옵션 선택 2</option>
           <option value="3">상품 상세 옵션 선택 3</option>
           <option value="4">상품 상세 옵션 선택 4</option>
@@ -116,7 +116,7 @@
         <tr>
           <th scope="col">작성자</th>
           <th scope="col">상품옵션</th>
-          <th scope="col">별점</th>
+          <th scope="col">평점</th>
           <th scope="col">내용</th>
           <th scope="col">등록일</th>
         </tr>
@@ -130,8 +130,7 @@
               from a media component.
             </div>
           </td>
-
-          <td class="col-1 text-center">⭐️⭐️⭐️⭐️⭐️</td>
+          <td class="col-1 text-center">1/5</td>
           <td class="col-4">
             <div class="align-items-center text-start">
               <div class="flex-grow-1">
@@ -158,6 +157,74 @@
       </tbody>
     </table>
   </div>
+  <div class="row" id="review-button">
+    <form>
+      <!-- Button trigger modal -->
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        후기 작성
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                상품 후기 작성
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <h3 class="fs-5 mb-2">상품 옵션</h3>
+              <select class="form-select mb-3">
+                <option value="1" selected>상품 상세 옵션 선택 1</option>
+                <option value="2">상품 상세 옵션 선택 2</option>
+                <option value="3">상품 상세 옵션 선택 3</option>
+                <option value="4">상품 상세 옵션 선택 4</option>
+              </select>
+              <h3 class="fs-5 mb-2">평점</h3>
+              <select class="form-select mb-3">
+                <option value="1" selected>1 (별로에요)</option>
+                <option value="2">2 (조금 아쉬워요)</option>
+                <option value="3">3 (괜찮아요)</option>
+                <option value="4">4 (좋아요)</option>
+                <option value="5">5 (아주 맘에 들어요)</option>
+              </select>
+              <div class="input-group">
+                <textarea
+                rows="10"
+                  class="form-control mb-3"
+                  placeholder="상품 후기를 입력하세요"
+                ></textarea>
+              </div>
+              <input class="mb-2" type="file" id="image1" accept="image/png, image/jpeg" />
+              <input class="mb-2" type="file" id="image2" accept="image/png, image/jpeg" />
+              <input class="mb-2" type="file" id="image3" accept="image/png, image/jpeg" />
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                닫기
+              </button>
+              <button type="submit" class="btn btn-primary">작성 완료</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
   <!-- 1:1 문의 탭 누르면 보이는 태그 -->
   <div class="row" id="product-qna">
     <table class="table">
@@ -167,23 +234,165 @@
           <th scope="col">제목</th>
           <th scope="col">비밀글</th>
           <th scope="col">등록일</th>
+          <th scope="col">답변일</th>
           <th scope="col">상태</th>
         </tr>
       </thead>
       <tbody class="table-group-divider align-middle">
         <tr>
-          <td class="col-2 text-center">OOO</td>
-          <td class="col-5">
-            <router-link to="/" class="router-link qna-link ms-3"
-              >1:1 문의 제목</router-link
+          <td class="col-1 text-center">OOO</td>
+          <td class="col-4">
+            <div
+              type="button"
+              class="ms-3 qna-link"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal-2"
             >
+              1:1 문의 제목
+            </div>
+            <!-- Modal -->
+            <div
+              class="modal fade"
+              id="exampleModal-2"
+              tabindex="-1"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                      1:1 문의합니다.(여기 제목 옆에 비밀글여부 바인딩 하면
+                      될거같은데)
+                    </h1>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body">
+                    <h2 class="fs-5">작성자</h2>
+                    <p class="text-muted">
+                      상품명 : 필통&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;옵션 : 파란색
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Impedit debitis, aperiam incidunt dolor odio magnam eum
+                      quod ratione veniam quo minima id suscipit quisquam
+                      tenetur facere eligendi placeat corrupti. Vitae.
+                    </p>
+                    <img
+                      src="https://via.placeholder.com/100x100?text=Image"
+                      class="img-thumbnail me-3"
+                    />
+                    <hr />
+                    <h2 class="fs-5">답변자</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Impedit debitis, aperiam incidunt dolor odio magnam eum
+                      quod ratione veniam quo minima id suscipit quisquam
+                      tenetur facere eligendi placeat corrupti. Vitae.
+                    </p>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      닫기
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </td>
           <td class="col-1 text-center">Y/N</td>
+          <td class="col-2 text-center">24/01/01</td>
           <td class="col-2 text-center">24/01/01</td>
           <td class="col-2 text-center">완료</td>
         </tr>
       </tbody>
     </table>
+  </div>
+  <div class="row" id="qna-button">
+    <form>
+      <!-- Button trigger modal -->
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal-3"
+      >
+        문의 작성
+      </button>
+      <div
+        class="modal fade"
+        id="exampleModal-3"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                1:1 문의합니다.(여기 제목 옆에 비밀글여부 바인딩 하면
+                될거같은데)
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <h2 class="fs-5">작성자</h2>
+              <p class="text-muted">
+                상품명 : 필통&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;옵션 : 파란색
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                debitis, aperiam incidunt dolor odio magnam eum quod ratione
+                veniam quo minima id suscipit quisquam tenetur facere eligendi
+                placeat corrupti. Vitae.
+              </p>
+              <img
+                src="https://via.placeholder.com/100x100?text=Image"
+                class="img-thumbnail me-3"
+              />
+              <hr />
+              <h2 class="fs-5">답변자</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                debitis, aperiam incidunt dolor odio magnam eum quod ratione
+                veniam quo minima id suscipit quisquam tenetur facere eligendi
+                placeat corrupti. Vitae.
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                닫기
+              </button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
+                수정
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 <script>
