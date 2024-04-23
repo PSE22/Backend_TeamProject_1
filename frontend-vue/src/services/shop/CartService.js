8f1df562bbc199d58373f8eb3d13b5214a651ec1
Gumiji <155078611+maruisdog@users.noreply.github.com>
2024-04-23 18:08:52 +0900

    Dev mj (#88)
    * myMJ
    
    * 카트 수정
    
    ---------
    
    Co-authored-by: PSE22 <114322247+PSE22@users.noreply.github.com>

---
+++ frontend-vue/src/services/shop/CartService.js
@@ -0,0 +1,15 @@
+import http from "@/utils/http-common"
+
+class CartService {
+    // 전체조회
+    getAll(cartId, page, size) {
+        return http.get(`/shop/cart?opId=${cartId}&page${page}&size${size}`);
+    }
+    
+    // 장바구니 저장함수
+    create(data) {
+        return http.post(`/shop/cart`, data);
+    }
+}
+
+export default new CartService();
