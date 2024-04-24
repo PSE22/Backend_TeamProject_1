9df2640f28c9e3e4bace770871facd3431b8d3d2
sjuk265 <sjuk265@gmail.com>
2024-04-24 20:07:56 +0900

    review, qna 생성

---
+++ BackendProject/src/main/java/org/example/backendproject/model/dto/shop/IQnaDto.java
@@ -0,0 +1,25 @@
+package org.example.backendproject.model.dto.shop;
+
+/**
+ * packageName : org.example.backendproject.model.dto.shop
+ * fileName : IQnaDto
+ * author : sjuk2
+ * date : 2024-04-24
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-24         sjuk2          최초 생성
+ */
+public interface IQnaDto {
+    Integer getUserId();
+    String getPdQnaTitle();
+    String getPdQnaContent();
+    String getPqAddDate();
+    String getPdQnaSecret();
+    String getPdQnaImgUrl();
+    String getPdQnaReplyContent();
+    String getQrAddDate();
+}
