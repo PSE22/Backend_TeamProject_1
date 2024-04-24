65d0fa7ef0fbeb518364ac5512dda04528e49962
michealMS <129148318+michealMS@users.noreply.github.com>
2024-04-24 16:03:08 +0900

    Dev ms (#94)
    * Login update
    
    Login update
    
    * Update Login
    
    Update Login
    
    * 로그인/회원가입 업데이트

--- frontend-vue/src/views/SignUpView.vue
+++ frontend-vue/src/views/SignUpView.vue
@@ -26,14 +26,13 @@
 
           <div class="col align-self-end">
             <div align="right">
-            <button type="button" id="idcheck" class="btn btn-secondary">
-              중복확인
-            </button>
+              <button type="button" id="idcheck" class="btn btn-secondary">
+                중복확인
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-
       <!-- 비밀번호 -->
       <div align="center">
         <div class="form-group col">
@@ -77,6 +76,27 @@
         </div>
       </div>
 
+      <!-- 성별 -->
+      <div class="form-check form-check-inline mt-3">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="gender"
+          value="M"
+          v-model="user.gender"
+        />
+        <label class="form-check-label" for="inlineRadio1">남</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="gender"
+          value="F"
+          v-model="user.gender"
+        />
+        <label class="form-check-label" for="inlineRadio2">여</label>
+      </div>
       <!-- 이메일 -->
       <div align="center">
         <div class="row">
@@ -107,12 +127,10 @@
 
           <div class="col align-self-end">
             <div align="right">
-            <button type="button" class="btn btn-secondary">
-              중복확인
-            </button>
+              <button type="button" class="btn btn-secondary">중복확인</button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
 
       <!-- <div align="center">
@@ -140,49 +158,100 @@
 
           <div class="col align-self-end">
             <div align="right">
-            <button type="button" id="phonecheck" class="btn btn-secondary">
-              인증
-            </button>
-          </div>
+              <button type="button" id="phonecheck" class="btn btn-secondary">
+                인증
+              </button>
+            </div>
+
           </div>
         </div>
+      </div>
+      <!-- 광고수신동의 -->
+      광고 수신 동의 :
+      <div class="form-check form-check-inline mt-3">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="userPromo"
+          value="Y"
+          v-model="user.userPromo"
+        />
+        <label class="form-check-label" for="promoYes">동의</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="userPromo"
+          value="N"
+          v-model="user.userPromo"
+        />
+        <label class="form-check-label" for="promoNo">미동의</label>
+      </div>
+
+      <br />
+      <!-- 계정분류 -->
+      <div class="form-check form-check-inline mt-3">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="userCode"
+          value="Y"
+          v-model="user.userCode"
+        />
+        <label class="form-check-label" for="promoYes">일반회원</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="flexRadioDisabled"
+          value="N"
+          v-model="user.userCode"
+          disabled
+        />
+        <label class="form-check-label" for="flexRadioDisabled">관리자</label>
       </div>
 
       <!-- 회원가입 -->
       <br />
       <div align="center">
-        <button type="button"
-         class="btn btn-primary btn-user mt-3 col-12"
-         @click="handleSignUp"
-         >
+        <button
+          type="button"
+          class="btn btn-primary btn-user mt-3 col-12"
+          @click="handleSignUp"
+        >
           회원가입
         </button>
       </div>
       <router-view />
     </form>
     <p v-if="message" class="alert alert-success" role="alert">
-      {{ message }} 
+      {{ message }}
     </p>
   </div>
 </template>
 <script>
-import LoginService from '@/services/login/LoginService';
+import LoginService from "@/services/login/LoginService";
 
 export default {
   // 데이터 바인딩
   data() {
     return {
       user: {
-                userId: "",
-                userPw: "",
-                rePw: "",
-                userName: "",
-                address: "",
-                userEmail: "",
-                userPhone: "",
-            },
-            message: "",      // 성공메세지 화면 출력속성
-    }
+        userId: "",
+        userPw: "",
+        rePw: "",
+        userName: "",
+        gender: "",
+        address: "",
+        userEmail: "",
+        userPhone: "",
+        userPromo: "",
+        userCode: "",
+      },
+      message: "", // 성공메세지 화면 출력속성
+    };
   },
   // TODO: 함수 정의
   methods: {
@@ -194,31 +263,33 @@ export default {
         // 공유저장소의 signUp 성공함수 실행
         this.$store.commit("signUpSuccess");
         this.message = "사용자가 등록되었습니다.";
-        // 로깅 
+        // 로깅
         console.log(response.data);
-      } catch(e) {
+      } catch (e) {
+
         // 공유저장소의 signUp 실패함수 실행
         this.$store.commit("signupFailure");
         this.message = "에러 : " + e;
         console.log(e);
       }
-    }
+    },
   },
   computed: {
     isPasswordMatch() {
       return this.user.userPw === this.user.rePw;
-    }
+    },
   },
 };
 </script>
 <style>
-.register-box {
+.signup-box {
   margin: 100px auto;
   background-color: rgba(255, 255, 255, 1);
   padding: 40px 30px;
   border: 3px solid #505050;
   width: 600px;
-  height: 650px;
+  height: 700px;
+
 }
 .a1 {
   position: relative;
@@ -229,15 +300,16 @@ export default {
 }
 
 @font-face {
-    font-family: 'YClover-Bold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_231029@1.1/YClover-Bold.woff2') format('woff2');
-    font-weight: 700;
-    font-style: normal;
+  font-family: "YClover-Bold";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_231029@1.1/YClover-Bold.woff2")
+    format("woff2");
+  font-weight: 700;
+  font-style: normal;
 }
 
 .logo {
-    /* background-color: rgb(115, 235, 67); */
-    font-size: 30px;
-    font-family: 'YClover-Bold';
+  /* background-color: rgb(115, 235, 67); */
+  font-size: 30px;
+  font-family: "YClover-Bold";
 }
 </style>
