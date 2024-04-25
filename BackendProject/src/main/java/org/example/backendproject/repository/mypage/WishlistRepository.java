d0d426cb6163168eb45333bdf090569706997bf9
toveny1 <toveny1@naver.com>
2024-04-25 19:11:18 +0900

    mywishlist4

--- BackendProject/src/main/java/org/example/backendproject/repository/mypage/WishlistRepository.java
+++ BackendProject/src/main/java/org/example/backendproject/repository/mypage/WishlistRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * packageName : org.example.backendproject.repository.mypage
@@ -24,17 +25,17 @@ import org.springframework.stereotype.Repository;
  * 2024-04-22         GGG          최초 생성
  */
 @Repository
-public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
-    @Query(value = "select * from (\n" +
-            "    SELECT PD.PD_ID AS pdId , PD.PD_NAME AS pdName, PD.PD_PRICE AS pdPrice, PI.PD_IMG_URL AS pdImgUrl \n" +
-            "    FROM TB_PRODUCT PD, TB_PRODUCT_IMAGE PI, TB_WISHLIST WS\n" +
-            "    WHERE PD.PD_ID = PI.PD_ID\n" +
-            "    AND PD.PD_ID = WS.PD_ID\n)"
+public interface WishlistRepository extends JpaRepository<Wishlist, PdIdUserIdPk> {
+    List<Wishlist> findByPdId(Integer pdId);
+    @Query(value = "SELECT PD.PD_ID AS pdId, PD.PD_NAME AS pdName, PD.PD_PRICE AS pdPrice, PI.PD_IMG_URL AS pdImgUrl " +
+            "FROM TB_PRODUCT PD " +
+            "JOIN TB_PRODUCT_IMAGE PI ON PD.PD_ID = PI.PD_ID " +
+            "JOIN TB_WISHLIST WS ON PD.PD_ID = WS.PD_ID " +
+            "WHERE WS.STATUS = 'Y'", nativeQuery = true)
 
-            , nativeQuery = true)
 
     Page<WishlistDto> selectWishlistContaining(
-            @Param("pdId") Integer pdId,
+            Integer pdId,
             Pageable pageable
     );
 
--- frontend-vue/src/services/mypage/MyWishListService.js
+++ frontend-vue/src/services/mypage/MyWishListService.js
@@ -13,16 +13,10 @@ class wishListService {
   }
 
 
-    // TODO: 추가(insert) 함수
-    // creat(data) {
-    //   // 사용법 : http.post("컨트롤러함수 url", 생성할객체 )
-    //   return http.post("/basic/dept", data);
-    // }
-
       // TODO: 삭제함수 : 부서번호(pdId)
-  // delete(pdId) { 
-  //   return http/delete(`/mypage/wishlist/deletion/${pdId}`)
-  // }
+  delete(pdId) { 
+    return http/delete(`/mypage/wishlist/deletion/${pdId}`)
+  }
 }
 
 
