3d8e2956859628b0b5f5f3a718afb8192399e017
Gumiji <155078611+maruisdog@users.noreply.github.com>
2024-04-29 19:29:37 +0900

    회원 장바구니 구현 (#122)
    * 카트수정
    
    * 중간 업데이트
    
    * 카트 수정
    
    * mj
    
    mj
    
    * 장바구니 로그인 권한 부여
    
    웹토큰에서 회원아이디를 가져와 장바구니 조회 (미완성)
    
    ---------
    
    Co-authored-by: PSE22 <114322247+PSE22@users.noreply.github.com>

--- frontend-vue/src/services/mypage/MyInquiryService.js
+++ frontend-vue/src/services/mypage/MyInquiryService.js
@@ -3,7 +3,7 @@ import http from "@/utils/http-common";
 
 class MyInquiryService {
   // 전체조회 공통함수
-  getAll( page, size) {
+  getAll(page, size) {
     return http.get(`/mypage/inquiry?page=${page}&size=${size}`);
   }
 }
