3b5e5d63b2d80fa23c172d0478b61778903bf06c
michealMS <chmstank912@gmail.com>
2024-04-30 09:07:45 +0900

    update_Coupon
    update_Coupon

--- frontend-vue/src/views/admin/AdminCouponView.vue
+++ frontend-vue/src/views/admin/AdminCouponView.vue
@@ -229,7 +229,7 @@
               <!-- DataTales Example -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">주문관리</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">쿠폰관리</h6>
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
