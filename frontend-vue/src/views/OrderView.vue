9e57d3145d7f2975cadd2bc43d9c1ad4179fe51f
PSE22 <shieun45@gmail.com>
2024-04-30 18:07:53 +0900

    OrderView 수정

--- frontend-vue/src/views/OrderView.vue
+++ frontend-vue/src/views/OrderView.vue
@@ -81,15 +81,15 @@
       <div class="order-sheet-content">
         <div class="row order-content-row">
           <div class="col-3 row-title">보내는 분</div>
-          <div class="col-9 row-content">(보내는 분)</div>
+          <div class="col-9 row-content">{{ user.userName }}</div>
         </div>
         <div class="row order-content-row">
           <div class="col-3 row-title">휴대전화</div>
-          <div class="col-9 row-content">(휴대전화)</div>
+          <div class="col-9 row-content">{{ user.userPhone }}</div>
         </div>
         <div class="row order-content-row">
           <div class="col-3 row-title">이메일</div>
-          <div class="col-9 row-content">(이메일)</div>
+          <div class="col-9 row-content">{{ user.userEmail }}</div>
         </div>
       </div>
     </div>
@@ -105,7 +105,7 @@
           <div class="col-9 row-content">
             <div class="form-check form-check-inline">
               <input
-                class="form-check-input"
+                class="form-check-input mt-1"
                 type="radio"
                 name="inlineRadioOptions"
                 value="option1"
@@ -116,7 +116,7 @@
             </div>
             <div class="form-check form-check-inline">
               <input
-                class="form-check-input"
+                class="form-check-input mt-1"
                 type="radio"
                 name="inlineRadioOptions"
                 value="option2"
@@ -127,7 +127,7 @@
             </div>
             <div class="form-check form-check-inline">
               <input
-                class="form-check-input"
+                class="form-check-input mt-1"
                 type="radio"
                 name="inlineRadioOptions"
                 value="option3"
@@ -168,18 +168,18 @@
         </div>
         <!-- 배송 정보 : 내용 : 받으시는 분 -->
         <div class="row order-content-row">
-          <div class="col-3 row-title">받으시는 분</div>
+          <div class="col-3 row-title pt-3">받으시는 분</div>
           <div class="col-9 row-content">
-            <div class="col-4">
+            <div class="col-4 ps-0">
               <input type="text" class="form-control" placeholder="이름" />
             </div>
           </div>
         </div>
         <!-- 배송 정보 : 내용 : 휴대전화 -->
         <div class="row order-content-row">
-          <div class="col-3 row-title">휴대전화</div>
+          <div class="col-3 row-title pt-3">휴대전화</div>
           <div class="col-9 row-content">
-            <div class="col-6">
+            <div class="col-6 ps-0">
               <input
                 type="text"
                 class="form-control"
@@ -190,10 +190,14 @@
         </div>
         <!-- 배송 정보 : 내용 : 배송 요청사항 -->
         <div class="row order-content-row">
-          <div class="col-3 row-title">배송 요청사항</div>
+          <div class="col-3 row-title pt-3">배송 요청사항</div>
           <div class="col-9 row-content">
-            <div class="col-8">
-                <textarea class="form-control" rows="4" placeholder="요청사항을 입력하세요."></textarea>
+            <div class="col-8 ps-0">
+              <textarea
+                class="form-control"
+                rows="4"
+                placeholder="요청사항을 입력하세요."
+              ></textarea>
             </div>
           </div>
         </div>
@@ -206,16 +210,18 @@
       <!-- 쿠폰 : 내용 -->
       <div class="order-sheet-content">
         <div class="row order-content-row">
-          <div class="col-3 row-title">쿠폰 적용</div>
+          <div class="col-3 row-title pt-3">쿠폰 적용</div>
           <div class="col-9 row-content">
             <div class="row">
-              <div class="col-3">
-                <input type="test" class="form-control" />
+              <div class="col-6">
+                <select class="form-select">
+                  <!-- 회원 보유 쿠폰 반복문 돌리기 -->
+                  <option selected>쿠폰선택</option>
+                  <option value="1">One</option>
+                </select>
               </div>
               <div class="col-3">
-                <button type="submit" class="btn btn-primary mb-3">
-                  우편번호찾기
-                </button>
+                <button type="submit" class="btn btn-primary">쿠폰선택</button>
               </div>
             </div>
           </div>
@@ -229,10 +235,19 @@
       <!-- 적립금 : 내용 -->
       <div class="order-sheet-content">
         <div class="row order-content-row">
-          <div class="col-3 row-title">적립금</div>
+          <div class="col-3 row-title pt-3">적립금</div>
           <div class="col-9 row-content">
-            <span class="col-9 row-content">사용가능 잔액 (input 태그) 원</span>
-            <button class="">모두 사용</button>
+            <div class="row">
+              <div class="col-3">
+                <input type="test" class="form-control" />
+              </div>
+              <div class="col-1 ps-0">
+                <p style="margin: 10px 0 0 0">원</p>
+              </div>
+              <div class="col-3">
+                <button type="submit" class="btn btn-primary">모두 사용</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -244,11 +259,41 @@
       <!-- 결제 수단 : 내용 -->
       <div class="order-sheet-content">
         <div class="row order-content-row">
-          <div class="col-3 row-title">결제 수단 선택</div>
+          <div class="col-3 row-title pt-3">결제 수단 선택</div>
           <div class="col-9 row-content">
-            <button class="">카카오페이</button>
-            <button class="">신용카드</button>
-            <button class="">간편결제</button>
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic radio toggle button group"
+            >
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio1"
+                autocomplete="off"
+                checked
+              />
+              <label class="btn mb-0" for="btnradio1">카카오페이</label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio2"
+                autocomplete="off"
+              />
+              <label class="btn mb-0" for="btnradio2">신용카드</label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio3"
+                autocomplete="off"
+              />
+              <label class="btn mb-0" for="btnradio3">간편결제</label>
+            </div>
           </div>
         </div>
       </div>
@@ -276,14 +321,60 @@
     </div>
     <!-- 결제 버튼 -->
     <div class="text-center">
-      <button class="btn btn-outline-dark btn-lg me-md-2 col-2" type="button">
+      <button
+        class="btn btn-warning btn-lg me-md-2 col-2 mt-5 mb-5"
+        type="button"
+      >
         결제하기
       </button>
     </div>
   </div>
 </template>
 <script>
-export default {};
+// import CartService from "@/services/shop/CartService";
+import UserService from "@/services/shop/UserService";
+
+export default {
+  data() {
+    return {
+      cart: [], // 장바구니 객체 배열
+      user: {}, // user 객체
+      userId: "",
+    };
+  },
+  methods: {
+    // 장바구니 전체조회
+    // async allCart(userId) {
+    //   try {
+    //     let response = await CartService.getAll(
+    //       userId,
+    //       this.page - 1,
+    //       this.pageSize
+    //     );
+    //     const { cart, totalItems } = response.data;
+    //     this.cart = cart;
+    //     this.count = totalItems;
+    //     // 로깅
+    //     console.log(response.data); // 웹브라우저 콘솔탬에 백앤드 데이터 표시
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // },
+    async getUser(userId) {
+      await UserService.getUser(userId)
+        .then((response) => {
+          this.user = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  mounted() {
+    this.getUser(this.$route.params.userId);
+  },
+};
 </script>
 <style>
 @import "@/assets/css/orderview.css";
