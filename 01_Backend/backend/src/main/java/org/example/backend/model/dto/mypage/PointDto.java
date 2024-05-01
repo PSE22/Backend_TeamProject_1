8b0685e82095adcfee08f1e2c74b4b846fd1deda
toveny1 <toveny1@naver.com>
2024-05-01 18:55:43 +0900

    mypageupdate

--- 01_Backend/backend/src/main/java/org/example/backend/model/dto/mypage/PointDto.java
+++ 01_Backend/backend/src/main/java/org/example/backend/model/dto/mypage/PointDto.java
@@ -1,31 +1,26 @@
 package org.example.backend.model.dto.mypage;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
- * packageName : org.example.backendproject.model.dto.mypage
+ * packageName : org.example.backend.model.dto.mypage
  * fileName : PointDto
  * author : GGG
- * date : 2024-04-26
+ * date : 2024-05-01
  * description :
  * 요약 :
  * <p>
  * ===========================================================
  * DATE            AUTHOR             NOTE
  * -----------------------------------------------------------
- * 2024-04-26         GGG          최초 생성
+ * 2024-05-01         GGG          최초 생성
  */
-@Getter
-@Setter
-public class PointDto {
-    private Integer usePointId;        // 사용 포인트 ID
-    private Integer pointId;           // 포인트 ID
-    private String userId;             // 유저 ID
-    private Integer pointBalance;      // 잔액
-    private String pointCode;          // 포인트 구분코드 (이벤트/리뷰/결제)
-    private String pointExpireStatus;  // 포인트 만료여부
-    private String addDate;            // 적립일
-    private String delDate;            // 삭제일
 
+public interface PointDto {
+    Integer usePointId();        // 사용 포인트 ID
+    Integer pointId();           // 포인트 ID
+    String userId();             // 유저 ID
+    Integer pointBalance();      // 잔액
+    String pointCode();          // 포인트 구분코드 (이벤트/리뷰/결제)
+    String pointExpireStatus();  // 포인트 만료여부
+    String addDate();            // 적립일
+    String delDate();            // 삭제일
 }
