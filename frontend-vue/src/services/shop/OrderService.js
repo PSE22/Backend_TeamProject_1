74dd66bfce81f946846bf8b23782a3095924ee0d
PSE22 <114322247+PSE22@users.noreply.github.com>
2024-05-03 09:00:52 +0900

    Update 주문페이지 (#144)

--- frontend-vue/src/services/shop/OrderService.js
+++ frontend-vue/src/services/shop/OrderService.js
@@ -11,5 +11,10 @@ class OrderService {
     getShipAddress(userId) {
         return http.get(`/shop/order/ship-address/${userId}`)
     }
+
+    // 회원 쿠폰 정보 조회
+    getUserCoupon(userId) {
+        return http.get(`/shop/order/user-coupon/${userId}`)
+    }
 }
 export default new OrderService();
