e29aab1c85291790a64aa10b141084b14859ba39
PSE22 <shieun45@gmail.com>
2024-05-08 01:31:04 +0900

    주문 페이지 수정

--- 01_Backend/backend/src/main/java/org/example/backend/repository/shop/PointRepository.java
+++ 01_Backend/backend/src/main/java/org/example/backend/repository/shop/PointRepository.java
@@ -26,13 +26,17 @@ import java.util.Optional;
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
     @Query(value = "SELECT \n" +
+            "    case \n" +
+            "        when ((select count(USE_POINT_ID) from TB_DETAIL_POINT WHERE USER_ID = :userId) != 0)\n" +
+            "then\n" +
             "(SELECT SUM(POINT_ADD)\n" +
             "FROM TB_POINT\n" +
-            "WHERE USER_ID = :userId \n" +
+            "WHERE USER_ID = :userId\n" +
             "AND STATUS = 'Y'\n" +
-            "GROUP BY USER_ID) \n" +
+            "GROUP BY USER_ID)\n" +
             "- \n" +
-            "((SELECT POINT_ADD\n" +
+            "(\n" +
+            "(SELECT POINT_ADD\n" +
             "FROM TB_POINT\n" +
             "WHERE POINT_ID = (\n" +
             "    SELECT POINT_ID\n" +
@@ -42,7 +46,7 @@ public interface PointRepository extends JpaRepository<Point, Long> {
             "        WHERE POINT_ID IN (\n" +
             "            SELECT POINT_ID\n" +
             "            FROM TB_POINT\n" +
-            "            WHERE USER_ID = :userId \n" +
+            "            WHERE USER_ID = :userId\n" +
             "            AND STATUS = 'Y'\n" +
             "        )\n" +
             "    )\n" +
@@ -56,13 +60,19 @@ public interface PointRepository extends JpaRepository<Point, Long> {
             "    WHERE POINT_ID IN (\n" +
             "        SELECT POINT_ID\n" +
             "        FROM TB_POINT\n" +
-            "        WHERE USER_ID = :userId \n" +
+            "        WHERE USER_ID = :userId\n" +
             "        AND STATUS = 'Y'\n" +
             "    )\n" +
             "    ORDER BY USE_POINT_ID DESC\n" +
             ")\n" +
             "WHERE ROWNUM = 1))\n" +
-            "AS resultPoint \n" +
+            "else \n" +
+            "(SELECT SUM(POINT_ADD)\n" +
+            "FROM TB_POINT\n" +
+            "WHERE USER_ID = :userId\n" +
+            "AND STATUS = 'Y'\n" +
+            "GROUP BY USER_ID)\n" +
+            "END AS resultPoint\n" +
             "FROM DUAL", nativeQuery = true)
     Optional<IPointDto> findByResultPoint(@Param("userId") String userId);
 }
