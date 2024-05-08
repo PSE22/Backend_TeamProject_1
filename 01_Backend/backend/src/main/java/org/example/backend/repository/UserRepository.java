12f63c06813fa383eb54bdc5a0c20ada3761a821
Gumiji <155078611+maruisdog@users.noreply.github.com>
2024-05-08 21:49:44 +0900

    마이페이지 메인 (#166)
    * 장바구니, 쿠폰함, 리뷰함, 문의함
    
    쿠폰,리뷰,문의 : 한 유저의 전체조회
    장바구니 마다 수량 가격 수정
    전체주문 버튼 구현
    
    * 마이페이지 메인
    
    마이페이지 메인 카운트, 헤더 비회원이 마이페이지 누르면 로그인 뷰로 이동

--- 01_Backend/backend/src/main/java/org/example/backend/repository/UserRepository.java
+++ 01_Backend/backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.example.backend.repository;
 
 import org.example.backend.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -24,6 +26,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
     List<User> findByStatusAndDelDate(String status, String delDate);
 
-//    값이 있을수도 있고 없을수도 있는 상황에 쓰는 클래스
-    Optional<User> findByUserId(String userId);
+    //    값이 있을수도 있고 없을수도 있는 상황에 쓰는 클래스
+    @Query(value = "SELECT USER_NAME FROM TB_USER " +
+            "WHERE USER_ID = :userId",
+            nativeQuery = true)
+    String findByUserName(@Param("userId") String userId);
 }
