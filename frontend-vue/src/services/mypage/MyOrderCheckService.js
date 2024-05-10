1103f0911e5e661dbb7c17a857849bd697c1f342
maruisdog <gumiji1202@gmail.com>
2024-05-10 18:48:17 +0900

    주문상세
    주문 상세 불러오기, 카트 css 수정

--- frontend-vue/src/services/mypage/MyOrderCheckService.js
+++ frontend-vue/src/services/mypage/MyOrderCheckService.js
@@ -5,12 +5,21 @@ class OrderCheck {
   // TODO: 전체 조회
   // 조회(select) -> get 방식 -> @GetMapping
   getAll(userId) {
-    return http.get(`/mypage/ordercheck/${userId}` ,{
-      headers: LoginHeader()
+    return http.get(`/mypage/ordercheck/${userId}`, {
+      headers: LoginHeader(),
     });
   }
 
+  getOrder(orderId) {
+    return http.get(`/mypage/orderdetail/${orderId}`, {
+      headers: LoginHeader(),
+    });
+  }
+  getOrderList(orderId) {
+    return http.get(`/mypage/orderlist/${orderId}`, {
+      headers: LoginHeader(),
+    });
+  }
 }
-
 
 export default new OrderCheck();
