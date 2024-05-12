c236dd0b5e07035c893011f711af0a63ed61dc6d
sjuk265 <sjuk265@gmail.com>
2024-05-12 19:31:37 +0900

    베스트 페이지 쿼리문 완성

--- 01_Backend/backend/src/main/java/org/example/backend/repository/shop/ProductRepository.java
+++ 01_Backend/backend/src/main/java/org/example/backend/repository/shop/ProductRepository.java
@@ -1,5 +1,6 @@
 package org.example.backend.repository.shop;
 
+import org.example.backend.model.dto.shop.IBestProductDto;
 import org.example.backend.model.dto.shop.IProductDto;
 import org.example.backend.model.entity.Product;
 
@@ -27,6 +28,51 @@ import java.util.List;
  */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+//    베스트 상품 전체 조회
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "(SELECT P.PD_ID, P.PD_NAME, P.PD_THUMBNAIL, P.ADD_DATE\n" +
+            "FROM TB_PRODUCT P, TB_OPTION O, TB_ORDER_DETAIL OD\n" +
+            "WHERE P.PD_ID = O.PD_ID\n" +
+            "AND O.OP_ID = OD.OP_ID\n" +
+            "AND P.STATUS = 'Y'\n" +
+            "AND OD.ORDER_DETAIL_CODE = 'OD01'\n" +
+            "GROUP BY P.PD_ID, P.PD_NAME, P.PD_THUMBNAIL, P.ADD_DATE\n" +
+            "ORDER BY SUM(OD.ORDER_DETAIL_CNT) DESC)\n" +
+            "WHERE ROWNUM <= 20"
+            , nativeQuery = true)
+    List<IBestProductDto> findAllBestProductOrderByAddDate();
+
+//    베스트 상품 전체 조회(높은 가격순)
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "(SELECT P.PD_ID AS pdId, P.PD_NAME AS pdName, P.PD_THUMBNAIL pdThumbnail, P.ADD_DATE AS addDate, P.PD_PRICE AS pdPrice\n" +
+            "FROM TB_PRODUCT P, TB_OPTION O, TB_ORDER_DETAIL OD\n" +
+            "WHERE P.PD_ID = O.PD_ID\n" +
+            "AND O.OP_ID = OD.OP_ID\n" +
+            "AND P.STATUS = 'Y'\n" +
+            "AND OD.ORDER_DETAIL_CODE = 'OD01'\n" +
+            "GROUP BY P.PD_ID, P.PD_NAME, P.PD_THUMBNAIL, P.ADD_DATE, P.PD_PRICE\n" +
+            "ORDER BY SUM(OD.ORDER_DETAIL_CNT) DESC, P.PD_PRICE DESC)\n" +
+            "WHERE ROWNUM <= 20"
+            , nativeQuery = true)
+    List<IBestProductDto> findAllBestProductOrderByAddDateAndPriceDesc();
+
+//    베스트 상품 전체 조회(낮은 가격순)
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "(SELECT P.PD_ID AS pdId, P.PD_NAME AS pdName, P.PD_THUMBNAIL pdThumbnail, P.ADD_DATE AS addDate, P.PD_PRICE AS pdPrice\n" +
+            "FROM TB_PRODUCT P, TB_OPTION O, TB_ORDER_DETAIL OD\n" +
+            "WHERE P.PD_ID = O.PD_ID\n" +
+            "AND O.OP_ID = OD.OP_ID\n" +
+            "AND P.STATUS = 'Y'\n" +
+            "AND OD.ORDER_DETAIL_CODE = 'OD01'\n" +
+            "GROUP BY P.PD_ID, P.PD_NAME, P.PD_THUMBNAIL, P.ADD_DATE, P.PD_PRICE\n" +
+            "ORDER BY SUM(OD.ORDER_DETAIL_CNT) DESC, P.PD_PRICE ASC)\n" +
+            "WHERE ROWNUM <= 20"
+            , nativeQuery = true)
+    List<IBestProductDto> findAllBestProductOrderByAddDateAndPrice();
+
 //    신상품 전체 조회
     @Query(value = "SELECT *\n" +
             "FROM (\n" +
@@ -37,7 +83,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             ")\n" +
             "WHERE ROWNUM <= 20"
     , nativeQuery = true)
-    List<Product> findAllOrderByAddDate();
+    List<Product> findAllNewProductOrderByAddDate();
 
 //    신상품 전체 조회(높은 가격순)
     @Query(value = "SELECT *\n" +
@@ -50,7 +96,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "WHERE ROWNUM <= 20\n" +
             "ORDER BY PD_PRICE DESC"
             , nativeQuery = true)
-    List<Product> findAllOrderByAddDateAndPriceDesc();
+    List<Product> findAllNewProductOrderByAddDateAndPriceDesc();
 
 //    신상품 전체 조회(낮은 가격순)
     @Query(value = "SELECT *\n" +
@@ -63,5 +109,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "WHERE ROWNUM <= 20\n" +
             "ORDER BY PD_PRICE ASC"
             , nativeQuery = true)
-    List<Product> findAllOrderByAddDateAndPrice();
+    List<Product> findAllNewProductOrderByAddDateAndPrice();
 }
