88824c7941f17f8dc87a9f3d0744e139bdf50454
Gumiji <155078611+maruisdog@users.noreply.github.com>
2024-05-13 19:22:22 +0900

    주문 상세페이지 (#187)
    * 장바구니, 쿠폰함, 리뷰함, 문의함
    
    쿠폰,리뷰,문의 : 한 유저의 전체조회
    장바구니 마다 수량 가격 수정
    전체주문 버튼 구현
    
    * 마이페이지 메인
    
    마이페이지 메인 카운트, 헤더 비회원이 마이페이지 누르면 로그인 뷰로 이동
    
    * 마이페이지 메인, 장바구니 수정
    
    마이페이지 메인의 주문정보 카운트 추가, 장바구니 선택삭제 오류 수정(체크 삭제)
    
    * 주문상세
    
    주문 상세 불러오기, 카트 css 수정
    
    * 주문 상세페이지
    
    주문 상세페이지 주문금액, 쿠폰, 포인트, 결제금액 / 주문 배송지 조회 오류 수정

--- frontend-vue/src/views/mypage/MyPageOrderDetail.vue
+++ frontend-vue/src/views/mypage/MyPageOrderDetail.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- 장바구니 타이틀 -->
     <h1 style="margin-top: 80px">주문 상세 내역</h1>
-    <h5 style="margin-bottom: 20px">{{ order.orAddDate }}</h5>
+    <h5 style="margin-bottom: 20px">{{ orderDetail.orAddDate }}</h5>
     <div>
       <!-- 장바구니 리스트 -->
       <table class="table text-center">
@@ -66,30 +66,33 @@
         <div class="order-sheet-content">
           <div class="row order-content-row">
             <div class="col-3 row-title">받는분</div>
-            <div class="col-9 row-content">{{ order.orderName }}</div>
+            <div class="col-9 row-content">{{ orderDetail.orderName }}</div>
           </div>
           <div class="row order-content-row">
             <div class="col-3 row-title">휴대전화</div>
-            <div class="col-9 row-content">{{ order.orderPhone }}</div>
+            <div class="col-9 row-content">{{ orderDetail.orderPhone }}</div>
           </div>
           <div class="row order-content-row">
             <div class="col-3 row-title">주소</div>
-            <div class="col-9 row-content">{{order.orderAddr}}</div>
+            <div class="col-9 row-content">{{ orderDetail.orderAddr }}</div>
           </div>
           <div class="row order-content-row">
             <div class="col-3 row-title">배송 요청사항</div>
-            <div class="col-9 row-content">{{order.shipMemo}}</div>
+            <div class="col-9 row-content">{{ orderDetail.shipMemo }}</div>
           </div>
           <div class="row order-content-row">
             <div class="col-3 row-title">결제수단</div>
-            <div class="col-9 row-content">{{ order.orderPayment }}</div>
+            <div class="col-9 row-content">{{ orderDetail.orderPayment }}</div>
           </div>
         </div>
       </div>
       <!-- 취소신청만 보임 -->
-      <div class="order-sheet-container" v-if="order.orderCode==='OD0201'||'OD0202'">
+      <div
+        class="order-sheet-container"
+        v-if="orderDetail.orderCode === 'OD0201' || orderDetail.orderCode === 'OD0202'"
+      >
         <!-- 적립금 사용 : 제목 -->
-        <div class="order-sheet-title" >취소/환불</div>
+        <div class="order-sheet-title">취소/환불</div>
         <!-- 적립금 사용 : 내용 -->
         <div class="order-sheet-content">
           <div class="row order-content-row">
@@ -97,7 +100,7 @@
             <div class="col-9 row-content">
               <div class="row row-cols-auto">
                 <div class="col ps-0">
-                  <p style="margin: 5px 0 0 0">{{ order.ocReason }}</p>
+                  <p style="margin: 5px 0 0 0">{{ orderDetail.ocReason }}</p>
                 </div>
               </div>
             </div>
@@ -105,9 +108,12 @@
         </div>
       </div>
       <!-- 환불신청만 보임 -->
-      <div class="order-sheet-container" v-else-if="order.orderCode==='OD0301'||'OD0302'">
+      <div
+        class="order-sheet-container"
+        v-else-if="orderDetail.orderCode === 'OD0301' || orderDetail.orderCode === 'OD0302'"
+      >
         <!-- 적립금 사용 : 제목 -->
-        <div class="order-sheet-title" >취소/환불</div>
+        <div class="order-sheet-title">취소/환불</div>
         <!-- 적립금 사용 : 내용 -->
         <div class="order-sheet-content">
           <div class="row order-content-row">
@@ -115,7 +121,9 @@
             <div class="col-9 row-content">
               <div class="row row-cols-auto">
                 <div class="col ps-0">
-                  <p style="margin: 5px 0 0 0">{{ order.refundReason }}</p>
+                  <p style="margin: 5px 0 0 0">
+                    {{ orderDetail.refundReason }}
+                  </p>
                 </div>
               </div>
             </div>
@@ -128,25 +136,25 @@
         <div class="card text-center mb-3" style="max-width: 18rem">
           <div class="card-header">주문금액</div>
           <div class="card-body">
-            <h3 class="card-title"> 1000 원</h3>
+            <h3 class="card-title">{{ totalPrice }} 원</h3>
           </div>
         </div>
         <div class="card text-center mb-3" style="max-width: 18rem">
-          <div class="card-header">할인금액</div>
+          <div class="card-header">쿠폰 할인금액</div>
           <div class="card-body">
-            <h3 class="card-title">{{ shipPrice }} 원</h3>
+            <h3 class="card-title">{{ useCoupon }} 원</h3>
           </div>
         </div>
         <div class="card text-center mb-3" style="max-width: 18rem">
           <div class="card-header">포인트 결제</div>
           <div class="card-body">
-            <h3 class="card-title">{{ totalPrice + shipPrice }} 원</h3>
+            <h3 class="card-title">{{ getUsePoint() }} P</h3>
           </div>
         </div>
         <div class="card text-center mb-3" style="max-width: 18rem">
           <div class="card-header">총 결제금액</div>
           <div class="card-body">
-            <h3 class="card-title">{{ totalPrice + shipPrice }} 원</h3>
+            <h3 class="card-title">{{ orderDetail.orderPrice }} 원</h3>
           </div>
         </div>
       </div>
@@ -154,7 +162,7 @@
         <button
           class="btn btn-outline-dark btn-lg me-md-2 col-2"
           type="button"
-          @click="sendOrderList"
+          @click="this.$router.push('/mypage/order')"
         >
           목록
         </button>
@@ -176,33 +184,98 @@ export default {
   data() {
     return {
       order: [],
+      orderDetail: {},
+      orderPrice: {},
       orderId: this.$route.params.orderId,
+      totalPrice: 0,
+      useCoupon: 0,
     };
   },
   methods: {
-    async getOrderId(orderId) {
+    // 주문 상품리스트
+    async getOrder(orderId) {
       try {
         let response = await MyOrderCheckService.getOrder(orderId);
         this.order = response.data; // 데이터를 order에 저장
+        this.getTotalPrice();
+        this.getUseCoupon();
+
         console.log(response.data); // 로그 출력
       } catch (e) {
         console.error(e); // 콘솔에 에러 출력
       }
     },
-    async getOrderList(orderId) {
+    // 배송지
+    async getOrderDetail(orderId) {
       try {
-        let response = await MyOrderCheckService.getOrderList(orderId);
-        this.order = response.data; // 데이터를 order에 저장
+        let response = await MyOrderCheckService.getOrderDetail(orderId);
+        this.orderDetail = response.data; // 데이터를 order에 저장
         console.log(response.data); // 로그 출력
       } catch (e) {
         console.error(e); // 콘솔에 에러 출력
       }
     },
-    
+
+    // 주문, 결제금액
+    async getOrderPrice(orderId) {
+      try {
+        let response = await MyOrderCheckService.getOrderPrice(orderId);
+        this.orderPrice = response.data; // 데이터를 order에 저장
+        console.log(response.data); // 로그 출력
+        this.getUseCoupon();
+      } catch (e) {
+        console.error(e); // 콘솔에 에러 출력
+      }
+    },
+    // 주문금액
+    async getTotalPrice() {
+      console.log("Order Array:", this.order);
+      try {
+        if (this.order.length > 0) {
+          //   this.totalPrice = this.order.reduce(
+          //     (acc, data) => acc + Number(data.orderDetailPrice)
+          //   );
+          //   누적합 : totalPrice = totalPrice + 값
+          for (const data of this.order) {
+            this.totalPrice = this.totalPrice + data.orderDetailPrice;
+          }
+          console.log("총금액", this.totalPrice);
+        } else {
+          this.totalPrice = 0;
+        }
+      } catch (error) {
+        console.error("Error in getTotalPrice:", error);
+        this.totalPrice = 0;
+      }
+    },
+    //   사용포인트
+    getUsePoint() {
+      if (this.orderPrice.usePointPrice) {
+        return -this.orderPrice.usePointPrice;
+      } else {
+        return 0;
+      }
+    },
+    //   사용쿠폰
+    async getUseCoupon() {
+      if (this.orderPrice.cpDcPrice) {
+         this.useCoupon = -this.orderPrice.cpDcPrice;
+        console.log("쿠폰",this.useCoupon)
+      } else if (this.orderPrice.cpDcRate) {
+        this.useCoupon = -this.totalPrice * this.orderPrice.cpDcRate;
+      } else {
+        this.useCoupon = 0;
+        console.log("쿠폰",this.useCoupon)
+
+      }
+    },
   },
+
   mounted() {
-    this.getOrderId(this.orderId); // 함수 호출 시 this.getOrderId.bind(this) 대신 this.getOrderId 사용
-    this.getOrderList(this.orderId); // 함수 호출 시 this.getOrderId.bind(this) 대신 this.getOrderId 사용
+    this.getUseCoupon();
+    this.getOrder(this.orderId);
+    this.getOrderDetail(this.orderId);
+    this.getOrderPrice(this.orderId);
   },
 };
 </script>
