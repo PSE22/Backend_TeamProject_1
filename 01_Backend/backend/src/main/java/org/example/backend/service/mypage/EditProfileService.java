812c3e3525d0e1a7558ee9454f9f70026ffdcc90
michealMS <chmstank912@gmail.com>
2024-05-13 17:54:25 +0900

    update_ProfileEdit
    update_ProfileEdit

--- 01_Backend/backend/src/main/java/org/example/backend/service/mypage/EditProfileService.java
+++ 01_Backend/backend/src/main/java/org/example/backend/service/mypage/EditProfileService.java
@@ -53,11 +53,12 @@ public class EditProfileService {
         return optionalUser.orElse(null);
     }
 
-    public void updateUser(User user) {
+    public User updateUser(User user) {
         userRepository.save(user);
+        return user;
     }
 
-    public void withdrawUser(String userPw) {
+    public boolean withdrawUser(String userPw) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String userId = userDetails.getUsername();
         Optional<User> optionalUser = userRepository.findById(userId);
@@ -71,6 +72,7 @@ public class EditProfileService {
         } else {
             throw new IllegalArgumentException("사용자를 찾을 수 없습니다.");
         }
+        return false;
     }
 
     @Scheduled(cron = "0 0 0 * * *")
--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -202,7 +202,7 @@ const routes = [
   },
   // 마이페이지 -> 회원정보 관리
   {
-    path: "/mypage/info",
+    path: "/mypage/editProfile",
     component: () => import("../views/mypage/MyPageInfo.vue"),
   },
 ];
